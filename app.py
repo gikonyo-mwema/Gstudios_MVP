e8ef8c80f93ed709eec525863fe2152fbfdc54ec
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-05-20 10:02:07 +0300

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__, static_url_path='/static')
 
@@ -6,6 +6,13 @@ app = Flask(__name__, static_url_path='/static')
 def hello_Gstudios():
     return render_template('index.html')
 
+@app.route("/api/services")
+def show_services():
+    return jsonify(services)
+
+@app.route("/api/login")
+def show_login():
+    return jsonify(login)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
