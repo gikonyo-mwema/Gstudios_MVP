56a201646039651ee931d0e9ec0f2d43baec9199
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-05-21 20:54:18 +0300

    update

--- app.py
+++ app.py
@@ -6,13 +6,18 @@ app = Flask(__name__, static_url_path='/static')
 def hello_Gstudios():
     return render_template('index.html')
 
-@app.route("/api/services")
-def show_services():
-    return jsonify(services)
+@app.route("/dashboard")
+def dashboard():
+    return render_template('dashboard.html')  
+    
 
-@app.route("/api/login")
-def show_login():
-    return jsonify(login)
+@app.route("/login")
+def login():
+    return render_template('login.html')
+ 
+@app.route("/sign_up")
+def signUp():
+    return render_template('sign_up.html')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
