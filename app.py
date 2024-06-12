a20f151ba8be89db521f568c70e6f0a64c673307
gikonyo-mwema <gikonyo.mwema@gmail.com>
2024-06-12 12:19:00 +0300

    correction signup route

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template
 from supabase import create_client
+from dotenv import load_dotenv
 import os
 
+load_dotenv()
 app = Flask(__name__, template_folder='templates')
 
 
@@ -15,14 +17,15 @@ def home():
     # Render the template with home page
     return render_template("index.html")
 
-
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', debug=True)
-
 @app.route("/sign_up")
 def sign_up():
+    # Render the template with sign_up page
     return render_template("sign_up.html")
 
 @app.route("/dashboard")
 def dashboard():
+    # Render the template with dashboard page
     return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
